3bc9008817584fce2e728f7e7cb734c67239aceb
Gabe Isman <gabe.isman@gmail.com>
2015-03-12 17:05:10 -0700

    Add chapter content underneath video. Indicate the active chapter.

---
+++ javascript/views/chapterdata.js
@@ -0,0 +1,107 @@
+define(function() {
+ return {
+    '119059448': {
+      title: 'The American Dream',
+      content: 'Through the examples of Irish and Puerto Rican immigration into Holyoke, participants will question: <b>what is the American Dream</b>?',
+      exercises: [
+        {
+          title: 'HIPP: What Is The American Dream?',
+          pageNum: '12'
+        }
+      ]
+    },
+
+    '119060790': {
+      title: 'Pursuit of Happiness',
+      content: 'In this lesson, participants will explore how they have the power to <b>achieve the American Dream</b> through education and social justice.',
+      exercises: [
+        {
+          title: 'HIPP: Moment of Silence',
+          pageNum: '15'
+        },
+        {
+          title: 'Brainstorm: What Is Success?',
+          pageNum: '15'
+        },
+        {
+          title: 'Discussion',
+          pageNum: '17'
+        }
+      ]
+    },
+    '119061107': {
+      title: 'School System',
+      content: 'School systems provide <b>opportunity</b> for upward mobility but suffer from socio-economic <b>disparities</b>. Here, participants will explore what it means to counteract social and economic <b>inequality</b>.',
+      exercises: [
+        {
+          title: 'HIPP: Violence Tree',
+          pageNum: '27'
+        },
+        {
+          title: 'HIPP: Restorative Justice',
+          pageNum: '29'
+        }
+      ]
+    },
+    '119062445': {
+      title: 'Drug Economy',
+      content: 'This violent, illegal, industry provides <b>economic mobility</b> in low-income communities. Participants will learn to recognize the <b>root causes of violence</b> and find non-violent solutions to conflict.',
+      exercises: [
+        {
+          title: 'Discussion: How to be an Ally (for mentors and educators)',
+          pageNum: '20'
+        },
+        {
+          title: 'HIPP: What is an ally?',
+          pageNum: '20'
+        },
+        {
+          title: 'HIPP: Who am I?',
+          pageNum: '22'
+        }
+      ]
+    },
+    '120338542': {
+      title: 'War on Drugs',
+      content: 'Police forces are fighting America’s longest running war in low-income communities. Here, participants will explore how <b>violence</b> works through discriminatory <b>law</b>.',
+      exercises: [
+        {
+          title: 'HIPP: What is Violence?',
+          pageNum: '33'
+        },
+        {
+          title: 'HIPP: Institutional Racism in History',
+          pageNum: '34'
+        }
+      ]
+    },
+    '119653722': {
+      title: 'Prison Industrial Complex',
+      content: 'Prisons outrank schools for public funding. With this lesson, participants will question decision-making on a larger level: <b>why is society making these choices?</b>',
+      exercises: [
+        {
+          title: 'HIPP: What is the School to Prison Pipeline?',
+          pageNum: '37'
+        },
+        {
+          title: 'HIPP Lift: Affirmative Posters',
+          pageNum: '39'
+        }
+      ]
+    },
+    '119653920': {
+      title: 'Where Do We Go From Here: Chaos or Community?',
+      content: 'What will you put your power towards—Chaos or Community? In this lesson, participants will recognize that <b>they each have power</b> to make choices and <b>determine the future of America</b>.',
+      exercises: [
+        {
+          title: 'HIPP: Concentric Circles',
+          pageNum: '41'
+        },
+        {
+          title: 'HIPP: The Power of One',
+          pageNum: '43'
+        }
+      ]
+    }
+  };
+});
--- javascript/views/film.js
+++ javascript/views/film.js
@@ -1,39 +1,62 @@
 define(
   [
     'views/formpopup',
+    'views/chapterdata',
+    'text!views/vimeo.txt',
+    'text!views/chapter.html',
     'text!views/film.html',
     'backbone'
   ],
-  function(FormPopup, template) {
+  function(FormPopup, chapterData, vimeoUrl, chapterTemplate, template) {
 
-  var Film = Backbone.View.extend({
-    // constructor: function() {} - omitted - defaults to superclass constructor
-    initialize: function() {
-      this.popup = new FormPopup();
-    },
+    var Film = Backbone.View.extend({
+      vimeoTemplate: _.template(vimeoUrl),
 
-    render: function() {
-      $(this.el).html(template);
-      return this;
-    },
+      template: _.template(template),
 
-    openPopup: function() {
-      this.popup.open();
-    },
+      videoId: '119016461',
 
-    events: {
-      'click .chapter': function(e) {
-        var videoUrl = $(e.currentTarget).data('video-url');
-        $('#watch-iframe').attr('src', videoUrl);
+      chapterData: chapterData,
+
+      chapterTemplate: _.template(chapterTemplate),
+
+      initialize: function() {
+        console.log(this.chapterData);
+        this.popup = new FormPopup();
       },
-      'click .access-link': 'openPopup',
-    },
 
-    className: 'view',
+      render: function() {
+        $(this.el).html(this.template({
+          vimeoUrl: this.vimeoTemplate({ videoId: this.videoId })
+        }));
+        return this;
+      },
 
-    id: 'film'
+      openPopup: function() {
+        this.popup.open();
+      },
 
-  });
+      events: {
+        'click .chapter': 'handleChapterClick',
+        'click .access-link': 'openPopup',
+      },
 
-  return Film;
+      handleChapterClick: function(e) {
+        var $currentTarget = $(e.currentTarget);
+        var videoId = $currentTarget.data('video-id');
+        var chapterDatum = this.chapterData[videoId];
+        $('#watch-iframe')
+          .attr('src', this.vimeoTemplate({ videoId: videoId }));
+        $('#curriculum-content').html(this.chapterTemplate(chapterDatum));
+        $('.chapter').removeClass('active');
+        $currentTarget.addClass('active');
+      },
+
+      className: 'view',
+
+      id: 'film'
+
+    });
+
+    return Film;
 });
