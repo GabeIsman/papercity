33ab7db06fdc83441dd2d4507a13e1541a0d52e1
Gabe Isman <gabe.isman@gmail.com>
2015-03-12 19:15:45 -0700

    Image urls.

--- javascript/views/chapterdata.js
+++ javascript/views/chapterdata.js
@@ -4,7 +4,7 @@ define(function() {
       title: 'The American Dream',
       duration: '3:39',
       content: 'Through the examples of Irish and Puerto Rican immigration into Holyoke, participants will question: <b>what is the American Dream</b>?',
-      thumbnailUrl: '/images/thumbs/american-dream.jpg',
+      thumbnailUrl: '/papercity/images/thumbs/american-dream.jpg',
       exercises: [
         {
           title: 'HIPP: What Is The American Dream?',
@@ -17,7 +17,7 @@ define(function() {
       title: 'Pursuit of Happiness',
       duration: '5:30',
       content: 'In this lesson, participants will explore how they have the power to <b>achieve the American Dream</b> through education and social justice.',
-      thumbnailUrl: '/images/thumbs/pursuit-of-happiness.jpg',
+      thumbnailUrl: '/papercity/images/thumbs/pursuit-of-happiness.jpg',
       exercises: [
         {
           title: 'HIPP: Moment of Silence',
@@ -37,7 +37,7 @@ define(function() {
       title: 'School System',
       duration: '9:45',
       content: 'School systems provide <b>opportunity</b> for upward mobility but suffer from socio-economic <b>disparities</b>. Here, participants will explore what it means to counteract social and economic <b>inequality</b>.',
-      thumbnailUrl: '/images/thumbs/school-system.jpeg',
+      thumbnailUrl: '/papercity/images/thumbs/school-system.jpeg',
       exercises: [
         {
           title: 'HIPP: Violence Tree',
@@ -53,7 +53,7 @@ define(function() {
       title: 'Drug Economy',
       duration: '8:01',
       content: 'This violent, illegal, industry provides <b>economic mobility</b> in low-income communities. Participants will learn to recognize the <b>root causes of violence</b> and find non-violent solutions to conflict.',
-      thumbnailUrl: '/images/thumbs/drug-economy.jpg',
+      thumbnailUrl: '/papercity/images/thumbs/drug-economy.jpg',
       exercises: [
         {
           title: 'Discussion: How to be an Ally (for mentors and educators)',
@@ -73,7 +73,7 @@ define(function() {
       title: 'War on Drugs',
       duration: '5:28',
       content: 'Police forces are fighting America’s longest running war in low-income communities. Here, participants will explore how <b>violence</b> works through discriminatory <b>law</b>.',
-      thumbnailUrl: '/images/thumbs/war-on-drugs.jpg',
+      thumbnailUrl: '/papercity/images/thumbs/war-on-drugs.jpg',
       exercises: [
         {
           title: 'HIPP: What is Violence?',
@@ -89,7 +89,7 @@ define(function() {
       title: 'Prison Industrial Complex',
       duration: '1:08',
       content: 'Prisons outrank schools for public funding. With this lesson, participants will question decision-making on a larger level: <b>why is society making these choices?</b>',
-      thumbnailUrl: '/images/thumbs/prison-industrial-complex.jpg',
+      thumbnailUrl: '/papercity/images/thumbs/prison-industrial-complex.jpg',
       exercises: [
         {
           title: 'HIPP: What is the School to Prison Pipeline?',
@@ -105,7 +105,7 @@ define(function() {
       title: 'Where Do We Go From Here: Chaos or Community?',
       duration: '8:16',
       content: 'What will you put your power towards—Chaos or Community? In this lesson, participants will recognize that <b>they each have power</b> to make choices and <b>determine the future of America</b>.',
-      thumbnailUrl: '/images/thumbs/chaos-or-community.jpg',
+      thumbnailUrl: '/papercity/images/thumbs/chaos-or-community.jpg',
       exercises: [
         {
           title: 'HIPP: Concentric Circles',
